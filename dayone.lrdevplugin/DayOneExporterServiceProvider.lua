75475f4f9dcf27ec7887820d6a768eb6bcc4c1b4
Philip Lundrigan <philiplundrigan@gmail.com>
2013-05-10 11:31:37 -0600

    WIP: Saving doentry file instead of using DayOne CLI
    A doentry file is created using UUID and the picture is renamed
    and moved.
    
    I still need to:
    - Finish tag support
    - Get location of journal from user

--- dayone.lrdevplugin/DayOneExporterServiceProvider.lua
+++ dayone.lrdevplugin/DayOneExporterServiceProvider.lua
@@ -1,6 +1,24 @@
 
+local random = math.random
+local function uuid()
+    local template ='xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'
+    return string.gsub(template, '[xy]', function (c)
+        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+        return string.format('%x', v)
+    end)
+end
+
+
 return {
     hideSections = { 'exportLocation', 'fileNaming' },
+    allowFileFormats = { 'JPEG' },
+
+    exportPresetFields = {
+        { key = 'use_time', default = true},
+        { key = 'use_keywords', default = false},
+        { key = 'use_specific_tags', default = false},
+        { key = 'tags', default = ""},
+    },
 
     sectionsForTopOfDialog = function ( viewFactory, propertyTable )
         local LrDialogs = import "LrDialogs"
@@ -8,12 +26,6 @@ return {
 
         local bind = LrView.bind
         local share = LrView.share
-
-        propertyTable.use_time = true
-        propertyTable.use_location = true
-        propertyTable.use_keywords = false
-        propertyTable.use_specific_tags = false
-        propertyTable.tags = ""
 
         return {
             {
@@ -24,14 +36,6 @@ return {
                     viewFactory:checkbox {
                         title = "Use picture's time",
                         value = bind 'use_time'
-                    },
-                },
-
-                viewFactory:row {
-                    spacing = viewFactory:control_spacing(),
-                    viewFactory:checkbox {
-                        title = "Use picture's location",
-                        value = bind 'use_location'
                     },
                 },
 
@@ -51,6 +55,7 @@ return {
                     },
                     viewFactory:edit_field {
                         value = bind 'tags', -- bound to property
+                        enabled = bind 'use_specific_tags',
                         immediate = true,
                     },
                 },
@@ -60,27 +65,20 @@ return {
 
     processRenderedPhotos = function ( functionContext, exportContext )
         local LrPathUtils = import 'LrPathUtils'
-        local LrFtp = import 'LrFtp'
         local LrFileUtils = import 'LrFileUtils'
-        local LrErrors = import 'LrErrors'
         local LrDialogs = import 'LrDialogs'
         local LrTasks = import 'LrTasks'
         local LrDate = import 'LrDate'
 
-        -- Make a local reference to the export parameters.
-
         local exportSession = exportContext.exportSession
         local exportParams = exportContext.propertyTable
 
-        -- Set progress title.
-
         local nPhotos = exportSession:countRenditions()
-
         local progressScope = exportContext:configureProgress {
-                            title = nPhotos > 1
-                                   and LOC( "$$$/FtpUpload/Upload/Progress=Adding ^1 photos to Day One", nPhotos )
-                                   or LOC "$$$/FtpUpload/Upload/Progress/One=Adding one photo to Day One",
-                        }
+                                title = nPhotos > 1
+                                        and "Adding " .. nPhotos .. " photos to Day One"
+                                        or "Adding one photo to Day One",
+        }
 
         -- Iterate through photo renditions.
 
@@ -89,35 +87,76 @@ return {
         for _, rendition in exportContext:renditions{ stopIfCanceled = true } do
 
             -- Wait for next photo to render.
-
             local success, pathOrMessage = rendition:waitForRender()
 
             -- Check for cancellation again after photo has been rendered.
-
             if progressScope:isCanceled() then break end
 
             if success then
-
                 local filename = LrPathUtils.leafName( pathOrMessage )
-                local date = rendition.photo:getRawMetadata(nil)["dateTimeOriginal"]
-                local command = "/usr/local/bin/dayone -d=\\\"" .. LrDate.timeToW3CDate(date) .. "\\\" -p=" .. pathOrMessage .. " new"
 
-                local success = LrTasks.execute(command)
+                local date = exportParams.use_time
+                             and rendition.photo:getRawMetadata("dateTimeOriginal")
+                             or LrDate.currentTime()
+
+                local old_keywords = exportParams.use_keywords
+                                     and rendition.photo:getFormattedMetadata("keywordTags")
+                                     or ''
+                local new_keywords = exportParams.use_specific_tags
+                                     and exportParams.tags
+                                     or ''
+                local keywords = old_keywords == ''
+                                 and new_keywords
+                                 or old_keywords .. ',' .. new_keywords
+
+                local uuid = uuid()
+
+                -- add support for keywords
+                -- check to make sure file does not exist
+                -- get location of journal
+
+
+                local entries = '/Users/philiplundrigan/Dropbox/Apps/Day One/Journal.dayone/entries'
+                local photos = '/Users/philiplundrigan/Dropbox/Apps/Day One/Journal.dayone/photos'
+
+                -- create photo
+                LrFileUtils.copy( pathOrMessage, LrPathUtils.child(LrPathUtils.standardizePath(photos), uuid .. '.jpg') )
+
+                -- create entry
+                local LrPathUtils = import "LrPathUtils"
+                local f = io.open(LrPathUtils.child(LrPathUtils.standardizePath(entries), uuid .. '.doentry'),"w")
+                f:write('<?xml version="1.0" encoding="UTF-8"?>\n')
+                f:write('<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n')
+                f:write('<plist version="1.0">\n')
+                f:write('   <dict>\n')
+                f:write('    <key>Creation Date</key>\n')
+                f:write('    <date>' .. LrDate.timeToW3CDate(date) .. 'Z</date>\n')
+                f:write('    <key>Entry Text</key>\n')
+                f:write('    <string>this is a test</string>\n')
+                f:write('    <key>Starred</key>\n')
+                f:write('    <false/>\n')
+
+                if exportParams.use_keywords or exportParams.use_specific_tags then
+                    f:write('<key>Tags</key>\n')
+
+                    f:write('<array>\n')
+                    f:write('    <string>' .. keywords .. '</string>\n')
+                    f:write('</array>\n')
+                end
+
+                f:write('    <key>UUID</key>\n')
+                f:write('    <string>' .. uuid .. '</string>\n')
+
+                f:write('</dict>\n')
+                f:write('</plist>\n')
+                f:close()
+
 
                 if not success then
-
-                    -- If we can't upload that file, log it.  For example, maybe user has exceeded disk
-                    -- quota, or the file already exists and we don't have permission to overwrite, or
-                    -- we don't have permission to write to that directory, etc....
-
                     table.insert( failures, filename )
                 end
 
-                -- When done with photo, delete temp file. There is a cleanup step that happens later,
-                -- but this will help manage space in the event of a large upload.
-
                 LrFileUtils.delete( pathOrMessage )
-
             end
 
         end
@@ -125,13 +164,12 @@ return {
         if #failures > 0 then
             local message
             if #failures == 1 then
-                message = LOC "$$$/FtpUpload/Upload/Errors/OneFileFailed=1 file failed to upload correctly."
+                message = "1 file failed to upload correctly."
             else
-                message = LOC ( "$$$/FtpUpload/Upload/Errors/SomeFileFailed=^1 files failed to upload correctly.", #failures )
+                message = #failures .. " files failed to upload correctly."
             end
             LrDialogs.message( message, table.concat( failures, "\n" ) )
         end
 
     end
-
 }
--- dayone.lrdevplugin/Info.lua
+++ dayone.lrdevplugin/Info.lua
@@ -2,12 +2,16 @@ return {
 
     LrSdkVersion = 3.0,
     LrToolkitIdentifier = 'com.philiplundrigan.dayone_export',
+
     LrPluginName = LOC "$$$/DayOneExport/PluginName=Day One Exporter",
+    LrPluginInfoUrl = 'http://www.github.com/philipbl',
 
     LrExportServiceProvider = {
         title = "Day One",
         file = 'DayOneExporterServiceProvider.lua',
     },
+
+    LrPluginInfoProvider = 'PluginInfoProvider.lua',
 
     VERSION = { major=1, minor=0, revision=0},
 
